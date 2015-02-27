9dd7325db35f66172623c7d5ae3f7284bf6035c2
Patrick Tsai <yinghau76@gmail.com>
2015-02-26 21:13:48 -0800

    Make it a system service

--- main.go
+++ main.go
@@ -2,26 +2,39 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"log"
 	"net/http"
 	"os"
 	"text/template"
+
+	"github.com/kardianos/service"
 )
 
-var phabricator = Phabricator{
-	Host: os.Getenv("PHABRICATOR_HOST"),
-	User: os.Getenv("PHABRICATOR_USER"),
-	Cert: os.Getenv("PHABRICATOR_CERT"),
+var logger service.Logger
+
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	go p.run()
+	return nil
 }
 
-var slack = Slack{
-	Token:    os.Getenv("SLACK_TOKEN"),
-	Username: "Phabricator",
-}
+func (p *program) run() {
+	var phabricator = Phabricator{
+		Host: os.Getenv("PHABRICATOR_HOST"),
+		User: os.Getenv("PHABRICATOR_USER"),
+		Cert: os.Getenv("PHABRICATOR_CERT"),
+	}
 
-func main() {
+	var slack = Slack{
+		Token:    os.Getenv("SLACK_TOKEN"),
+		Username: "Phabricator",
+	}
+
 	if err := phabricator.Connect(); err != nil {
-		log.Fatalln("Failed to connect Phabricator: ", err.Error())
+		logger.Error("Failed to connect Phabricator: ", err.Error())
+		return
 	}
 
 	channel := os.Getenv("SLACK_CHANNEL")
@@ -33,17 +46,64 @@ func main() {
 		text := r.FormValue("storyText")
 		author := r.FormValue("storyAuthorPHID")
 		phid := r.FormValue("storyData[objectPHID]")
-		log.Println("New story:", story, author, phid, text)
+		logger.Info("New story:", story, author, phid, text)
 
 		if phobj, err := phabricator.PhidQuery(phid); phobj != nil {
 			var msg bytes.Buffer
 			t.Execute(&msg, struct{ Uri, Name, Text string }{phobj["uri"], phobj["name"], text})
 			slack.postMessage(channel, msg.String())
 		} else {
-			log.Println("Error:", err.Error())
+			logger.Error("Error:", err.Error())
 			slack.postMessage(channel, text)
 		}
 	})
 
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
+
+func (p *program) Stop(s service.Service) error {
+	return nil
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "phabricator-to-slack",
+		DisplayName: "phabricator-to-slack",
+		Description: "Passing Phabricator notifications to Slack",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	flag.Parse()
+	if args := flag.Args(); len(args) > 0 {
+		verb := args[0]
+		switch verb {
+		case "install":
+			err = s.Install()
+			if err != nil {
+				log.Fatalln("Failed to install:", err)
+			}
+			log.Printf("Service \"%s\" installed.\n", svcConfig.DisplayName)
+		case "uninstall":
+			err = s.Uninstall()
+			if err != nil {
+				log.Fatalln("Failed to uninstall:", err)
+			}
+			log.Printf("Service \"%s\" uninstalled.\n", svcConfig.DisplayName)
+		}
+		return
+	}
+
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
 }
