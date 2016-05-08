d9125f9f791ca17ebb3074b19b0ae171c499acd8
Patrick Tsai <yinghau76@gmail.com>
2016-05-07 18:43:31 -0700

    Update to use new token-based authetication

--- phabricator.go
+++ phabricator.go
@@ -1,18 +1,18 @@
 package ph2slack
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"time"
 )
 
 // PhObject represents attributes of a Phabricator object
 type PhObject map[string]string
+
+// PhObjects represents collection of PhObject
+type PhObjects map[string]PhObject
 
 // ConduitError represents an error happened when communicating with Phabricator
 type ConduitError struct {
@@ -26,96 +26,49 @@ func (c ConduitError) Error() string {
 
 // Phabricator provides API access to a Phabricator site specified by Host, User, and Cert.
 type Phabricator struct {
-	Host string
-	User string
-	Cert string
-
-	conduitJSON string // conduit is necessary for subsequent API calls
-}
-
-// Connect connects to specified Phabricator site
-func (p *Phabricator) Connect() error {
-	// Prepare connect parameters
-	token, signature := p.genAuthTokenAndSignature()
-	paramsJSON, _ := json.Marshal(map[string]interface{}{
-		"client":        "Bot",
-		"clientVersion": 0,
-		"user":          p.User,
-		"host":          p.Host,
-		"authToken":     token,
-		"authSignature": signature,
-	})
-
-	resp, err := http.PostForm(p.Host+"/api/conduit.connect", url.Values{
-		"params":      {string(paramsJSON)},
-		"output":      {"json"},
-		"__conduit__": {"true"},
-	})
-	if err != nil {
-		return err
-	}
-
-	var result struct {
-		ErrorCode string `json:"error_code"`
-		ErrorInfo string `json:"error_info"`
-		Result    struct {
-			ConnectionID float64 `json:"connectionID"`
-			SessionKey   string  `json:"sessionKey"`
-			UserPHID     string  `json:"userPHID"`
-		} `json:"result"`
-	}
-	resultJSON, _ := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err = json.Unmarshal(resultJSON, &result); err != nil {
-		return err
-	}
-	if result.ErrorCode != "" {
-		return ConduitError{Code: result.ErrorCode, Info: result.ErrorInfo}
-	}
-
-	conduitJSON, _ := json.Marshal(map[string]interface{}{
-		"sessionKey":   result.Result.SessionKey,
-		"connectionID": result.Result.ConnectionID,
-	})
-
-	// Keep the conduit for later API calls
-	p.conduitJSON = string(conduitJSON)
-	return nil
-}
-
-func (p *Phabricator) genAuthTokenAndSignature() (int64, string) {
-	token := time.Now().Unix()
-	sum := sha1.Sum([]byte(fmt.Sprintf("%d%s", token, p.Cert)))
-	return token, hex.EncodeToString(sum[:])
+	Host  string
+	Token string
 }
 
 // PhidQuery queries attributes of a Phabricator object by its ID
 func (p *Phabricator) PhidQuery(phid string) (PhObject, error) {
+	conduitJSON, _ := json.Marshal(map[string]interface{}{
+		"token": p.Token,
+	})
+
 	resp, err := http.PostForm(p.Host+"/api/phid.query", url.Values{
 		"params[phids]":       {fmt.Sprintf(`["%s"]`, phid)},
-		"params[__conduit__]": {p.conduitJSON},
+		"params[__conduit__]": {string(conduitJSON)},
 		"output":              {"json"},
 	})
 	if err != nil {
 		return nil, err
 	}
 
+	result, err := p.resultOrError(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, obj := range result {
+		return obj, nil // expect only one
+	}
+	return nil, fmt.Errorf("Empty result")
+}
+
+func (p *Phabricator) resultOrError(resp *http.Response) (PhObjects, error) {
 	var result struct {
 		ErrorCode string              `json:"error_code"`
 		ErrorInfo string              `json:"error_info"`
 		Result    map[string]PhObject `json:"result"`
 	}
-	resultJSON, _ := ioutil.ReadAll(resp.Body)
+	body, _ := ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
-	if err = json.Unmarshal(resultJSON, &result); err != nil {
+	if err := json.Unmarshal(body, &result); err != nil {
 		return nil, err
 	}
 	if result.ErrorCode != "" {
 		return nil, ConduitError{Code: result.ErrorCode, Info: result.ErrorInfo}
 	}
-
-	for _, obj := range result.Result {
-		return obj, nil // expect only one
-	}
-	return nil, fmt.Errorf("Empty result from %s", resultJSON)
+	return result.Result, nil
 }
--- phabricator_test.go
+++ phabricator_test.go
@@ -1,17 +1,44 @@
 package ph2slack
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInvalidPhabricatorConnect(t *testing.T) {
+func TestPhabricatorInvalidToken(t *testing.T) {
 	ph := &Phabricator{
-		Host: "invalid",
-		User: "invalid",
-		Cert: "invalid",
+		Host:  "https://secure.phabricator.com",
+		Token: "01234567890123456789012345678901",
 	}
-	err := ph.Connect()
+	_, err := ph.PhidQuery(os.Getenv("PHABRICATOR_TEST_PHID"))
 	assert.Error(t, err)
+}
+
+func TestPhabricatorInvalidPhid(t *testing.T) {
+	ph := &Phabricator{
+		Host:  "https://secure.phabricator.com",
+		Token: os.Getenv("PHABRICATOR_TEST_TOKEN"),
+	}
+	_, err := ph.PhidQuery("invalid")
+	assert.Error(t, err)
+}
+
+func TestPhabricatorInvalidHost(t *testing.T) {
+	ph := &Phabricator{
+		Host:  "invalid",
+		Token: os.Getenv("PHABRICATOR_TEST_TOKEN"),
+	}
+	_, err := ph.PhidQuery(os.Getenv("PHABRICATOR_TEST_PHID"))
+	assert.Error(t, err)
+}
+
+func TestPhabricatorConnect(t *testing.T) {
+	ph := &Phabricator{
+		Host:  os.Getenv("PHABRICATOR_TEST_HOST"),
+		Token: os.Getenv("PHABRICATOR_TEST_TOKEN"),
+	}
+	_, err := ph.PhidQuery(os.Getenv("PHABRICATOR_TEST_PHID"))
+	assert.NoError(t, err)
 }
